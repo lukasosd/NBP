a959a3c022cce7945b75b7669ea5be78aa6299e8
Łukasz <smietki94@gmail.com>
2022-12-21 21:40:28 +0100

    Bug fix(date format)

--- functions.py
+++ functions.py
@@ -23,8 +23,8 @@ def cur_import(currency, date):
 
 
 def day_before(date):
-    date = date.replace('/', '-').replace('.', '-')
-    date_split = date.split('-')
+    date = date.replace('/', '.')
+    date_split = date.split('.')
     date = date_split[2] + '-' + date_split[1] + '-' + date_split[0]
     date_time_obj = datetime.strptime(date, '%Y-%m-%d')
     calculation = str(date_time_obj - timedelta(days=1))
