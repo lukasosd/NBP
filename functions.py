a342dc94f3b369d71f9f8b1a01f34076218e1495
Łukasz <smietki94@gmail.com>
2022-12-21 17:17:52 +0100

    Initial commit

---
+++ functions.py
@@ -0,0 +1,29 @@
+import requests
+from datetime import datetime, timedelta
+
+def cur_import(currency, date):
+    while True:
+        url = f'https://api.nbp.pl/api/exchangerates/rates/a/{currency}/{date}'
+        response = requests.get(url)
+        if response.status_code == 404:
+            date = day_before(date)
+        else:
+            break
+
+    data = response.json()
+    exchange_rate = data['rates'][0]['mid']
+    return exchange_rate
+
+
+def day_before(date):
+    date_time_obj = datetime.strptime(date, '%Y-%m-%d')
+    calculation = str(date_time_obj - timedelta(days=1))
+    return calculation[:10]
+
+def result(exchange_rate, amount):
+    amount = float(amount.replace(',','.'))
+    exchange_rate = float(exchange_rate)
+    text = f'''Kurs:{exchange_rate}
+    Calculated money: {amount*exchange_rate}
+    '''
+    return text
---
+++ main.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import functions
+
+
+def calculate(currency, date, amount):
+    new_date = functions.day_before(date)
+    exchange_rate = functions.cur_import(currency,new_date)
+    message = functions.result(exchange_rate, amount)
+    global text
+    text = st.text_area('Result', value=message)
+
+st.set_page_config(layout="wide")
+
+st.markdown(""" <style> .font {
+font-size:60px ; font-family: 'Cooper Black'; color: #3230b8;} 
+</style> """, unsafe_allow_html=True)
+st.markdown('<p class="font">CURRENCY CONVERTER</p>', unsafe_allow_html=True)
+st.subheader("This app downloads the exchange rate from  the NBP website and "
+             "calculates the given amount.")
+st.write("Please insert invoice date below:")
+date = st.text_input(label="", placeholder="YYYY-MM-DD", key='invoice_date')
+st.write("Please insert currency below:")
+currency = st.text_input(label="", placeholder="E.g. EUR", key='currency')
+st.write("Please insert amount below:")
+amount = st.text_input(label="", placeholder="E.g. 10000.00", key='amount')
+
+if (st.button('Submit')):
+    calculate(currency, date, amount)
+
+
